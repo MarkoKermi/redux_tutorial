efaca7605a99fb9d7bbd78c09bf591b412829b51
Marko Kermiciev <kermicievmarko@gmail.com>
2023-02-28 09:16:39 +0100

    redux tutorial setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar";
+import CardContainer from "./components/cardContainer";
+import {useDispatch, useSelector} from 'react-redux'
+import {calculateTotals} from './features/cart/cartSlice'
+import { useEffect } from "react";
+
+
+
+function App() {
+  const {cartItems} = useSelector ((store) => store.cart)
+  const dispatch =useDispatch ()
+
+  useEffect (() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+  return <main>
+    <Navbar/>
+    <CardContainer/>
+    </main>;
+}
+export default App;
